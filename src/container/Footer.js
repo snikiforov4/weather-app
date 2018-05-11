74b21ce66da9e86405032c2089aefead4191acbb
Serhii Nykyforov <04mailbox@gmail.com>
2018-05-11 16:19:52 +0300

    Remove LastUpdateInfo component

--- src/container/Footer.js
+++ src/container/Footer.js
@@ -1,11 +1,9 @@
 import React, {Component} from 'react';
-import LastUpdateInfo from '../LastUpdateInfo';
 
 class Footer extends Component {
   render () {
     return (
       <footer className="page-footer">
-        <LastUpdateInfo/>
         <div className="footer-copyright">
           <div className="container">
             &copy; 2018 Serhii Nykyforov
