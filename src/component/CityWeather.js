ee7e5e13ab1566881280feb3553d4b305b5255d5
Serhii Nykyforov <04mailbox@gmail.com>
2018-05-13 13:10:10 +0300

    Use correct units (Kelvin) for temperature

--- src/component/CityWeather.js
+++ src/component/CityWeather.js
@@ -5,6 +5,7 @@ class CityWeather extends Component {
     const weather = this.props.weather;
     const weatherIcon = `https://openweathermap.org/img/w/${weather.weather[0].icon}.png`;
     const favoriteIcon = this.props.favorite ? 'favorite' : 'favorite_border';
+    const temperatureSign = '\u212A';
     return (
       <div className="card">
         <div className="card-content">
@@ -19,9 +20,9 @@ class CityWeather extends Component {
             <div className="col s2"><img src={weatherIcon} alt="weather icon"/></div>
             <div className="col s10 left-align">
               <p className="grey-text text-lighten-1">{weather.weather[0].main}</p>
-              <p>{weather.main.temp} &#8457;</p>
-              <p>Min temperature: {weather.main.temp_min} &#8457;</p>
-              <p>Max temperature: {weather.main.temp_max} &#8457;</p>
+              <p>{weather.main.temp} {temperatureSign}</p>
+              <p>Min temperature: {weather.main.temp_min} {temperatureSign}</p>
+              <p>Max temperature: {weather.main.temp_max} {temperatureSign}</p>
               <p>Humidity: {weather.main.humidity}%</p>
               <p>Wind: {weather.wind.speed}m/s</p>
             </div>
